674c8c2a87e70a7a722f76d94e384e256d7232c9
Kabir <kabir@nunet.io>
2020-10-22 11:51:32 +0200

    first experimental version by Amante

---
+++ build/precache-manifest.d8b0d7ecab620a881ca0458d08d00a8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc005eec1fb3556ebd6629e97f7e260",
+    "url": "/index.html"
+  },
+  {
+    "revision": "465c678f68383d41a3c1",
+    "url": "/static/css/main.1561aeb8.chunk.css"
+  },
+  {
+    "revision": "dde6f21165a00ee4c7c6",
+    "url": "/static/js/2.4251dc3b.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.4251dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "465c678f68383d41a3c1",
+    "url": "/static/js/main.90ccfbcc.chunk.js"
+  },
+  {
+    "revision": "bc053751b98981c2cd99",
+    "url": "/static/js/runtime-main.67f8c521.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
