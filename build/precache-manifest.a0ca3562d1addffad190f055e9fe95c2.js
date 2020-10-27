06652ec5d2b9fb7aa68bc2f66202f138a6e0c0e4
bulidiriba <bulidiriba@gmail.com>
2020-10-27 12:01:54 +0300

    update the build folder

---
+++ build/precache-manifest.a0ca3562d1addffad190f055e9fe95c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213486fd6c750dd4b43d532f291a04f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "557dfe0675568266d5c4",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "adc0254cb8cf5fe73a2f",
+    "url": "/static/js/2.8833a1cc.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/static/js/2.8833a1cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557dfe0675568266d5c4",
+    "url": "/static/js/main.997e18f2.chunk.js"
+  },
+  {
+    "revision": "6315054bca489e7d88fa",
+    "url": "/static/js/runtime-main.afab30f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8b0d7ecab620a881ca0458d08d00a8c.js"
+  "/precache-manifest.a0ca3562d1addffad190f055e9fe95c2.js"
 );
 
 self.addEventListener('message', (event) => {
